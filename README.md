4dc685712e544dcf8db97b477f7eaa3446ea8ca8
MihiThilina <90233553+MihiThilina@users.noreply.github.com>
2022-11-22 12:34:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# effector-vite-react-template-l2fz8e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/effector-vite-react-template-l2fz8e)
---
+++ src/application.tsx
@@ -0,0 +1,15 @@
+import { useUnit } from 'effector-react';
+import viteLogo from '/vite.svg';
+import reactLogo from './assets/react.svg';
+import './application.css';
+import { $counter, buttonClicked } from './model';
+
+export function App() {
+  const [counter, handleClick] = useUnit([$counter, buttonClicked]);
+
+  return (
+    <div className="App">
+      <h1>helo</h1>
+    </div>
+  );
+}
